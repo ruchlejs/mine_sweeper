38d53588197742c80c8ca797fb4452038e9d71ba
Sacha Ruchlejmer <sushi.sacha.r@gmail.com>
2024-11-09 11:25:22 +0100

    First commit

---
+++ src/MainApp.vue
@@ -0,0 +1,26 @@
+<template>
+    <BandeauComponent />
+</template>
+
+<script>
+import BandeauComponent from './components/BandeauComponent.vue'
+
+export default {
+    name: 'MainComponent',
+    components: {
+        BandeauComponent
+    }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
